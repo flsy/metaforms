776b2f1634b7a47d2d5c4e5db14d7fe2be4b8a67
Honza Smid <jansmidhonza@gmail.com>
2019-12-13 15:14:16 +0100

    0.0.5

--- src/__tests__/fields.spec.ts
+++ src/__tests__/fields.spec.ts
@@ -1,40 +1,40 @@
-import { FieldType, getFormData, hasError, maxlength, pattern, required, setFieldValue, validateForm } from "..";
-import { compose } from "fputils";
+import { FieldType, getFormData, hasError, maxlength, pattern, required, setFieldValue, validateForm } from '..';
+import { compose } from 'fputils';
 
 const fields: FieldType[] = [
   {
-    type: "text",
-    name: "name",
-    validation: [required("fill name")]
+    type: 'text',
+    name: 'name',
+    validation: [required('fill name')],
   },
   {
-    type: "number",
-    name: "age",
-    validation: [required("fill age"), maxlength({ message: "max 3 digits", value: 3 })]
+    type: 'number',
+    name: 'age',
+    validation: [required('fill age'), maxlength({ message: 'max 3 digits', value: 3 })],
   },
   {
-    type: "datetime-local",
-    name: "born",
-    validation: [required("fill DOB"), pattern({ message: "wrong date format", value: "^[0-9]+$" })]
-  }
+    type: 'datetime-local',
+    name: 'born',
+    validation: [required('fill DOB'), pattern({ message: 'wrong date format', value: '^[0-9]+$' })],
+  },
 ];
 
-describe("fields behaviour", () => {
-  it("returns errors when fields are empty", () => {
+describe('fields behaviour', () => {
+  it('returns errors when fields are empty', () => {
     const result = validateForm(fields);
 
     expect(hasError(result)).toEqual(true);
 
-    expect(result[0].errorMessage).toEqual("fill name");
-    expect(result[1].errorMessage).toEqual("fill age");
-    expect(result[2].errorMessage).toEqual("fill DOB");
+    expect(result[0].errorMessage).toEqual('fill name');
+    expect(result[1].errorMessage).toEqual('fill age');
+    expect(result[2].errorMessage).toEqual('fill DOB');
   });
 
-  it("returns errors when fields filled with wrong values", () => {
+  it('returns errors when fields filled with wrong values', () => {
     const filled = compose(
-      setFieldValue("name", "Joel"),
-      setFieldValue("age", "Joel"),
-      setFieldValue("born", "xxx"),
+      setFieldValue('name', 'Joel'),
+      setFieldValue('age', 'Joel'),
+      setFieldValue('born', 'xxx'),
     )(fields);
 
     const result = validateForm(filled);
@@ -42,15 +42,15 @@ describe("fields behaviour", () => {
     expect(hasError(result)).toEqual(true);
 
     expect(result[0].errorMessage).toEqual(undefined);
-    expect(result[1].errorMessage).toEqual("max 3 digits");
-    expect(result[2].errorMessage).toEqual("wrong date format");
+    expect(result[1].errorMessage).toEqual('max 3 digits');
+    expect(result[2].errorMessage).toEqual('wrong date format');
   });
 
-  it("returns no errors when fields are properly filled", () => {
+  it('returns no errors when fields are properly filled', () => {
     const filled = compose(
-      setFieldValue("name", "Joel"),
-      setFieldValue("age", 50),
-      setFieldValue("born", '20')
+      setFieldValue('name', 'Joel'),
+      setFieldValue('age', 50),
+      setFieldValue('born', '20'),
     )(fields);
 
     const result = validateForm(filled);
@@ -61,6 +61,6 @@ describe("fields behaviour", () => {
     expect(result[1].errorMessage).toEqual(undefined);
     expect(result[2].errorMessage).toEqual(undefined);
 
-    expect(getFormData(filled)).toEqual({ name: "Joel", age: 50, born: '20' });
+    expect(getFormData(filled)).toEqual({ name: 'Joel', age: 50, born: '20' });
   });
 });
