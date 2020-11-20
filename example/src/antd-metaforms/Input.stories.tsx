1b0a6cc2894767bf261f7a57d82e921aa451f61e
Jakub Kolebaba <jakub@kolebaba.cz>
2020-11-20 22:29:28 +0100

    Updated builder

--- example/src/antd-metaforms/Input.stories.tsx
+++ example/src/antd-metaforms/Input.stories.tsx
@@ -3,9 +3,10 @@ import {storiesOf} from "@storybook/react";
 import FormTemplate from "./FormTemplate";
 import {AntBuilder} from "build-metaforms";
 
-const builder = new AntBuilder()
-  .addInput({ name: 'name', label: 'Text input', placeholder: "Write something" })
-  .addSubmit({ name: 'submit', label: 'Submit' })
+const form = new AntBuilder()
+  .addInput('name')
+  .addSubmit('submit', { label: 'Save' })
+  .build()
 
-storiesOf('antd-metaforms', module).add('input', () => <FormTemplate defaultForm={builder.build()}/>);
+storiesOf('antd-metaforms', module).add('input', () => <FormTemplate defaultForm={form}/>);
 
--- packages/build-metaforms/src/index.ts
+++ packages/build-metaforms/src/index.ts
@@ -1,24 +1,24 @@
 class Builder {
   fields: { [key:string]: any } = {};
 
-  addField = ({ name, ...props }: { name: string, [key: string]: any}) => {
-    this.fields[name] = props;
+  addField = (name: string, type: string, props: { [key: string]: any}) => {
+    this.fields[name] = { type, ...props };
     return this;
   }
 
   build = () => this.fields;
 }
 
-type InputProps = { name: string, label: string, placeholder: string };
-type SubmitProps = { name: string, label: string };
+type InputProps = { label: string, placeholder: string };
+type SubmitProps = { label: string };
 
 export class AntBuilder extends Builder {
-  addInput = (p: InputProps) => {
-    this.addField({ type: 'input', ...p });
+  addInput = (name: string, p?: InputProps) => {
+    this.addField(name, 'input', p);
     return this;
   }
-  addSubmit = (p: SubmitProps) => {
-    this.addField(({ type: 'submit', ...p }));
+  addSubmit = (name, p?: SubmitProps) => {
+    this.addField(name, 'submit', p);
     return this;
   }
 }
